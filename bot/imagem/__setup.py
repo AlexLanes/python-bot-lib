247e705c5fabedc58cf688bb1add6e8dfa14ae24
Alex <alex_lanes@hotmail.com>
2023-12-27 19:46:39 -0300

    Adicionado suporte para bytes

--- bot/imagem/__setup.py
+++ bot/imagem/__setup.py
@@ -1,4 +1,5 @@
 # std
+from io import BytesIO
 from time import perf_counter
 # interno
 import bot
@@ -12,6 +13,13 @@ from PIL import Image
 pyscreeze.USE_IMAGE_NOT_FOUND_EXCEPTION = False
 
 
+def transformar_pillow (imagem: str | Image.Image | bytes) -> Image.Image:
+    """Receber o formato esperado e transformar para uma imagem do `pillow`"""
+    if isinstance(imagem, str): return Image.open(imagem)
+    if isinstance(imagem, bytes): return Image.open(BytesIO(imagem))
+    return imagem
+
+
 def capturar_tela (regiao: Coordenada = None, cinza=False) -> Image.Image:
     """Realizar uma captura de tela
     - `regiao` especifica uma parte da tela
@@ -20,12 +28,13 @@ def capturar_tela (regiao: Coordenada = None, cinza=False) -> Image.Image:
     return imagem.convert("L") if cinza else imagem
 
 
-def procurar_imagem (imagem: bot.tipagem.caminho | Image.Image, confianca: bot.tipagem.PORCENTAGENS = "0.9", segundos=0, regiao: Coordenada = None, cinza=False) -> Coordenada | None:
+def procurar_imagem (imagem: bot.tipagem.caminho | Image.Image | bytes, confianca: bot.tipagem.PORCENTAGENS = "0.9", segundos=0, regiao: Coordenada = None, cinza=False) -> Coordenada | None:
     """Procurar a `imagem` na tela, com `confianca`% de confiança na procura e na `regiao` da tela informada
-    - `imagem` caminho até o arquivo ou `Image` do `pillow`
+    - `imagem` pode ser o caminho até o arquivo, bytes da image ou `Image` do módulo `pillow`
     - `regiao` especifica uma parte da tela
     - `segundos` tempo de procuraa pela imagem
     - `cinza` compara ambas imagem como grayscale"""
+    imagem = transformar_pillow(imagem)
     box = pyscreeze.locateOnScreen(
         image=imagem, 
         minSearchTime=segundos,
@@ -36,11 +45,12 @@ def procurar_imagem (imagem: bot.tipagem.caminho | Image.Image, confianca: bot.t
     return Coordenada(box.left, box.top, box.width, box.height) if box else None
 
 
-def procurar_imagens (imagem: bot.tipagem.caminho | Image.Image, confianca: bot.tipagem.PORCENTAGENS = "0.9", regiao: Coordenada = None, cinza=False) -> list[Coordenada] | None:
+def procurar_imagens (imagem: bot.tipagem.caminho | Image.Image | bytes, confianca: bot.tipagem.PORCENTAGENS = "0.9", regiao: Coordenada = None, cinza=False) -> list[Coordenada] | None:
     """Procurar todas as vezes que a `imagem` aparece na tela, com `confianca`% de confiança na procura e na `regiao` da tela informada
-    - `imagem` caminho até o arquivo ou `Image` do `pillow`
+    - `imagem` pode ser o caminho até o arquivo, bytes da image ou `Image` do módulo `pillow`
     - `regiao` especifica uma parte da tela
     - `cinza` compara ambas imagem como grayscale"""
+    imagem = transformar_pillow(imagem)
     boxes = list(pyscreeze.locateAllOnScreen(imagem, 
                                              grayscale=cinza,
                                              confidence=confianca,
@@ -56,13 +66,13 @@ def procurar_imagens (imagem: bot.tipagem.caminho | Image.Image, confianca: bot.
     return coordenadas
 
 
-def cores_imagem (imagem: bot.tipagem.caminho | Image.Image, limite: int | slice = 10) -> list[tuple[int, tuple[int, int, int]]]:
+def cores_imagem (imagem: bot.tipagem.caminho | Image.Image | bytes, limite: int | slice = 10) -> list[tuple[int, tuple[int, int, int]]]:
     """Obter as cores RGB e frequencia de cada pixel da `imagem`
-    - `imagem` pode ser o caminho até o arquivo ou `Image` do módulo `pillow`
+    - `imagem` pode ser o caminho até o arquivo, bytes da image ou `Image` do módulo `pillow`
     - `limite` quantidade que será retornada dos mais frequentes
     - `for (frequencia, cor) in cores_imagem()`"""
     if limite != None and limite <= 0: return []
-    imagem = Image.open(imagem) if isinstance(imagem, str) else imagem # abrir imagem se for string
+    imagem = transformar_pillow(imagem)
     itens: list[tuple[int, tuple[int, int, int]]] = imagem.getcolors(10000) # extrair cores
     itens.sort(key=lambda item: item[0], reverse=True) # ordernar pelos mais frequentes
     limite = limite if isinstance(limite, slice) else slice(limite) # criar o slice de limite
@@ -100,14 +110,14 @@ class LeitorOCR:
         bot.logger.debug(f"Extração realizada em {perf_counter() - inicio:.2f} segundos")
         return extracoes
 
-    def ler_imagem (self, imagem: bot.tipagem.caminho | Image.Image) -> list[tuple[str, Coordenada]]:
+    def ler_imagem (self, imagem: bot.tipagem.caminho | Image.Image | bytes) -> list[tuple[str, Coordenada]]:
         """Extrair texto e coordenadas de uma imagem
-        - `imagem` pode ser o camnho até o arquivo ou `Image` do módulo `pillow`
+        - `imagem` pode ser o caminho até o arquivo, bytes da image ou `Image` do módulo `pillow`
         - `for texto, coordenada in leitor.ler_imagem()`"""
         inicio = perf_counter()
         bot.logger.debug("Iniciado o processo de extração de textos e coordenadas de uma imagem")
         
-        imagem: Image.Image = Image.open(imagem) if isinstance(imagem, str) else imagem
+        imagem = transformar_pillow(imagem)
         extracoes = self.__extrair(imagem)
 
         bot.logger.debug(f"Extração realizada em {perf_counter() - inicio:.2f} segundos")
