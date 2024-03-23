7d5a255beafa33d2d273326860f8354cf363b4bc
Alex Lanes Angelo <alex_lanes@hotmail.com>
2024-03-23 17:45:12 -0300

    Comentário configfile

--- bot/configfile/__init__.py
+++ bot/configfile/__init__.py
@@ -1,6 +1,7 @@
 """Módulo para inicialização do `configparse`
 - Arquivo(s) `.ini` deve(m) estar presente(s) na raiz do projeto
 - Usar variável com a sintaxe `${secao:opcao}`
-- `$` é reservado, utilizar `$$` para contornar"""
+- `$` é reservado, utilizar `$$` para contornar
+- `#` comenta a linha se estiver no começo"""
 
 from .__setup import *
