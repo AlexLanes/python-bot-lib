5f8d0c24a876f5a53e72f60ec67897751ba0cf0b
Alex Lanes Angelo <alex_lanes@hotmail.com>
2024-03-24 04:09:57 -0300

    Comentário

--- bot/argumentos/__init__.py
+++ bot/argumentos/__init__.py
@@ -1,3 +1,6 @@
-"""Módulo para tratar os argumentos de inicialização do Python"""
+"""Módulo para tratar os argumentos de inicialização do Python
+- Argumentos posicionais devem vir antes dos nomeados
+- Argumentos nomeados: `--nome valor`
+- Cobrir argumentos com aspas, caso possua espaço"""
 
 from .__setup import *
