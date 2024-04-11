b6ee2cd6d7eeea79b58cf2ddb45c4e1abe8fd072
Alex Lanes <alex_lanes@hotmail.com>
2024-04-10 21:43:35 -0300

    Alterado módulo dos argumentos para uma versão com genérics

--- bot/argumentos/__setup.py
+++ bot/argumentos/__setup.py
@@ -1,15 +1,13 @@
 # std
 from sys import argv
 # interno
-import bot
+from bot import logger
+from bot.tipagem import primitivo
 
 
 argumentos = argv[1:] # primeiro argumento é sempre o nome do arquivo iniciado
 posicionais: list[str] = []
-nomeados: dict[str, str] = {}
-
-
-class ArgumentosError (Exception): pass
+nomeados: dict[str, str | None] = {}
 
 
 def inicializar_argumentos () -> None:
@@ -18,36 +16,49 @@ def inicializar_argumentos () -> None:
     while argumentos and not argumentos[0].startswith("--"):
         posicionais.append(argumentos.pop(0))
 
-    if len(argumentos) <= 1: return
-
     # nomeados
-    index = -1
-    while index < len(argumentos) - 2:
-        index += 1
-        argumento, proximo = argumentos[index], argumentos[index + 1]
-        if not argumento.startswith("--") or proximo.startswith("--"): continue
-        nomeados[argumento.lstrip("-")] = proximo
+    for index in range(0, len(argumentos) - 1):
+        atual, proximo = argumentos[index], argumentos[index + 1]
+        if not atual.startswith("--"): continue
+        nomeados[atual.lstrip("-")] = proximo if not proximo.startswith("-") else None
 
 
-def obter_argumento_nomeado (nome: str, default="", obrigatorio=False) -> str:
+def transformar_tipo[T: primitivo] (valor: str, tipo: type[T]) -> T:
+    """Fazer a transformação do `valor` para `type(tipo)`
+    - Helper"""
+    match str(tipo):
+        case "<class 'str'>": return valor
+        case "<class 'int'>" | "<class 'float'>": return tipo(valor)
+        case "<class 'bool'>": return valor.lower().strip() == "true"
+        case _: return None
+
+
+def nomeado_existe (nome: str) -> bool:
+    """Checar se o argumento posicional existe"""
+    return nome in nomeados
+
+
+def nomeado_ou[T: primitivo] (nome: str, default: T = "") -> T:
     """Obter o valor do argumento `nome` ou `default`
-    - `ArgumentosError` caso `obrigatorio` e não encontrado"""
-    if nome not in nomeados and obrigatorio: 
-        raise ArgumentosError(f"Argumento nomeado obrigatório '{nome}' não encontrado")
-    return nomeados.get(nome, default)
+    - Função `genérica`"""
+    return transformar_tipo(nomeados[nome], type(default)) \
+        if nomeado_existe(nome) else default
 
 
-def obter_argumento_posicional (index: int, default="", obrigatorio=False) -> str:
+def posicional_existe (index: int) -> bool:
+    """Checar se o argumento posicional existe"""
+    return posicionais and index < len(posicionais)
+
+
+def posicional_ou[T: primitivo] (index: int, default: T = "") -> T:
     """Obter o valor do argumento no `index` ou `default`
-    - `ArgumentosError` caso `obrigatorio` e não encontrado"""
-    valido = posicionais and index < len(posicionais)
-    if not valido and obrigatorio: 
-        raise ArgumentosError(f"Argumento posicional obrigatório '{index}' não encontrado")
-    return posicionais[index] if valido else default
+    - Função `genérica`"""
+    return transformar_tipo(posicionais[index], type(default)) \
+        if posicional_existe(index) else default
 
 
 inicializar_argumentos()
-bot.logger.informar(
+logger.informar(
     f"""Argumento(s) de inicialização encontrado(s)
         Posicionais: {posicionais}
         Nomeados: {nomeados}"""
@@ -55,7 +66,8 @@ bot.logger.informar(
 
 
 __all__ = [
-    "ArgumentosError",
-    "obter_argumento_nomeado",
-    "obter_argumento_posicional"
+    "nomeado_ou",
+    "posicional_ou",
+    "nomeado_existe",
+    "posicional_existe"
 ]
