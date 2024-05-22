74d2594cdc7f647d0d15aea37473e3f4ca551ea1
Alex Lanes <alex_lanes@hotmail.com>
2024-05-22 08:41:40 -0300

    Melhorias no bot.mouse e remoção de função não utilizada

--- bot/mouse/__setup.py
+++ bot/mouse/__setup.py
@@ -1,13 +1,15 @@
 # std
 from time import sleep
-from typing import Literal
 # interno
 import bot
 from bot.estruturas import Coordenada
 # externo
 from pyscreeze import pixel
 from pynput.mouse import Controller, Button
-from win32gui import GetCursorInfo as get_cursor_info
+from win32api import (
+    GetCursorPos as get_cursor_position, 
+    SetCursorPos as set_cursor_position
+)
 
 
 mouse = Controller()
@@ -15,7 +17,7 @@ mouse = Controller()
 
 def posicao_mouse () -> tuple[int, int]:
     """Obter a posição (X, Y) do mouse"""
-    return mouse.position
+    return get_cursor_position()
 
 
 def obter_x_y (coordenada: tuple[int, int] | Coordenada | None) -> tuple[int, int]:
@@ -27,28 +29,24 @@ def obter_x_y (coordenada: tuple[int, int] | Coordenada | None) -> tuple[int, in
     return posicao_mouse()
 
 
-def mover_mouse (coordenada: tuple[int, int] | Coordenada, delay=0.0) -> None:
-    """Mover o mouse até a `coordenada`
-    - `delay` é necessário para aguardar atualizar o tipo do ponteiro"""
-    delay /= 2
-    mouse.position = obter_x_y(coordenada)
-    # atualizar o ponteiro
-    mouse.move(1, -1); sleep(delay)
-    mouse.move(-1, 1); sleep(delay)
+def mover_mouse (coordenada: tuple[int, int] | Coordenada) -> None:
+    """Mover o mouse até a `coordenada`"""
+    coordenada = obter_x_y(coordenada)
+    set_cursor_position(coordenada)
+    mouse.position = coordenada
 
 
 def clicar_mouse (coordenada: Coordenada | tuple[int, int] = None, 
                   botao: bot.tipagem.BOTOES_MOUSE = "left", 
-                  quantidade=1, delay=0.5) -> None:
+                  quantidade=1, delay=0.1) -> None:
     """Clicar com o `botão` do mouse na `coordenada` `quantidade` vezes
     - Default `coordenada`: posição atual do mouse"""
-    if coordenada: mover_mouse(coordenada, delay=delay) # mover mouse se requisitado
-    botao: Button = Button[botao] # Enum do botão
-    mouse.click(botao, max(1, quantidade)) # clicar
+    if coordenada: mover_mouse(coordenada) # mover mouse se requisitado
+    mouse.click(Button[botao], max(1, quantidade))
     sleep(delay)
 
 
-def scroll_vertical (quantidade: int, direcao: bot.tipagem.DIRECOES_SCROLL = "baixo", delay=0.02) -> None:
+def scroll_vertical (quantidade: int, direcao: bot.tipagem.DIRECOES_SCROLL = "baixo", delay=0.05) -> None:
     """Realizar o scroll vertical na posição atual do mouse `quantidade` vezes na `direcao` informada"""
     quantidade = max(1, quantidade)
     for _ in range(quantidade):
@@ -62,17 +60,10 @@ def cor_mouse () -> tuple[int, int, int]:
     return pixel(*posicao_mouse())
 
 
-def tipo_ponteiro () -> Literal["normal", "clicavel", "texto", "indefinido"]:
-    """Obter o tipo do ponteiro atual do mouse"""
-    return { 65541: "texto", 65539: "normal", 65567: "clicavel" } \
-        .get(get_cursor_info()[1], "indefinido")
-
-
 __all__ = [
     "cor_mouse",
     "mover_mouse",
     "clicar_mouse",
-    "tipo_ponteiro",
     "posicao_mouse",
     "scroll_vertical"
 ]
