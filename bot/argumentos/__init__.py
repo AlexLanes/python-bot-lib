c4498e4cb116ee500051a3ac7a58e86e887deb0a
Alex Lanes <alex_lanes@hotmail.com>
2024-06-26 15:45:27 -0300

    Refatoramento do módulo argumentos

--- bot/argumentos/__init__.py
+++ bot/argumentos/__init__.py
@@ -1,6 +1,8 @@
-"""Módulo para tratar os argumentos de inicialização do Python
+"""Módulo para tratar argumentos de inicialização do Python
 - Argumentos posicionais devem vir antes dos nomeados
 - Argumentos nomeados: `--nome valor`
-- Cobrir argumentos com aspas, caso possua espaço"""
+- Cobrir argumentos com aspas, caso possua espaço
+
+Exemplo: `python main.py posicional0 posicional1 --nome "Alex Lanes"`"""
 
 from .__setup import *
--- bot/argumentos/__setup.py
+++ bot/argumentos/__setup.py
@@ -3,14 +3,13 @@ from sys import argv
 # interno
 import bot
 
-
 argumentos = argv[1:] # primeiro argumento é sempre o nome do arquivo iniciado
 posicionais: list[str] = []
 nomeados: dict[str, str | None] = {}
 
-
-def inicializar_argumentos () -> None:
-    """Inicializar os argumentos posicionais e nomeados"""
+def inicializar_argumentos () -> bool:
+    """Inicializar os argumentos posicionais e nomeados
+    - Retorna indicação se há algum argumento"""
     # posicionais
     while argumentos and not argumentos[0].startswith("--"):
         posicionais.append(argumentos.pop(0))
@@ -19,40 +18,36 @@ def inicializar_argumentos () -> None:
     for index in range(0, len(argumentos) - 1):
         atual, proximo = argumentos[index], argumentos[index + 1]
         if not atual.startswith("--"): continue
-        nomeados[atual.lstrip("-")] = proximo if not proximo.startswith("-") else None
+        nomeados[atual.lstrip("-").lower()] = proximo if not proximo.startswith("-") else None
 
-
-def nomeado_existe (nome: str) -> bool:
-    """Checar se o argumento posicional existe"""
-    return nome in nomeados
-
-
-def nomeado_ou[T: bot.tipagem.primitivo] (nome: str, default: T = "") -> T:
-    """Obter o valor do argumento `nome` ou `default`
-    - Função `genérica`"""
-    return bot.util.transformar_tipo(nomeados[nome], type(default)) \
-        if nomeado_existe(nome) else default
-
+    return bool(posicionais or nomeados)
 
 def posicional_existe (index: int) -> bool:
     """Checar se o argumento posicional existe"""
-    return posicionais and index < len(posicionais)
+    try: return posicionais[index] != None
+    except IndexError: return False
 
+def nomeado_existe (nome: str) -> bool:
+    """Checar se o argumento posicional existe"""
+    return nome.strip().lower() in nomeados
 
 def posicional_ou[T: bot.tipagem.primitivo] (index: int, default: T = "") -> T:
     """Obter o valor do argumento no `index` ou `default`
-    - Função `genérica`"""
-    return bot.util.transformar_tipo(posicionais[index], type(default)) \
-        if posicional_existe(index) else default
+    - Transforma o tipo do argumento para o mesmo tipo do `default` informado"""
+    return default if not posicional_existe(index) else \
+        bot.util.transformar_tipo(posicionais[index], type(default))
 
+def nomeado_ou[T: bot.tipagem.primitivo] (nome: str, default: T = "") -> T:
+    """Obter o valor do argumento `nome` ou `default` caso não exista
+    - Transforma o tipo do argumento para o mesmo tipo do `default` informado"""
+    nome = nome.lower()
+    return default if not nomeado_existe(nome) else \
+        bot.util.transformar_tipo(nomeados[nome], type(default))
 
-inicializar_argumentos()
-bot.logger.informar(
-    f"""Argumento(s) de inicialização encontrado(s)
-        Posicionais: {posicionais}
-        Nomeados: {nomeados}"""
-) if posicionais or nomeados else None
-
+# inicializar no primeiro `import` do pacote
+if inicializar_argumentos():
+    mensagem = f"Argumento(s) de inicialização encontrado(s)\n\tPosicionais: {posicionais}\n\tNomeados: {nomeados}"
+    bot.logger.informar(mensagem)
 
 __all__ = [
     "nomeado_ou",
