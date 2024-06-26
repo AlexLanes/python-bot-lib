21cea07c619d9844bacf7307f197ecdaa67c576d
Alex Lanes <alex_lanes@hotmail.com>
2024-06-26 16:07:31 -0300

    Refatoramento do módulo configfile

--- bot/configfile/__init__.py
+++ bot/configfile/__init__.py
@@ -1,7 +1,22 @@
-"""Módulo para inicialização do `configparse`
-- Arquivo(s) `.ini` deve(m) estar presente(s) na raiz do projeto
-- Usar variável com a sintaxe `${secao:opcao}`
+"""Módulo para inicialização de variáveis a partir de arquivo de configuração
+- Arquivo terminado em `.ini` deve estar presente na raiz do projeto
+- Para concatenação de valores, utilizar a sintaxe `${opção} ou ${seção:opção}`
 - `$` é reservado, utilizar `$$` para contornar
-- `#` comenta a linha se estiver no começo"""
+- `#` comenta a linha se estiver no começo
+
+Exemplo:
+```
+[logger]
+flag_debug = False
+flag_persistencia = False
+dias_persistencia = 14
+
+[FTP]
+host = test.rebex.net
+user = demo 
+password = password
+timeout = 5
+port = 21
+```"""
 
 from .__setup import *
--- bot/configfile/__setup.py
+++ bot/configfile/__setup.py
@@ -6,41 +6,41 @@ from bot.tipagem import primitivo
 from bot.util import transformar_tipo
 from bot.windows import diretorio_execucao, nome_base
 
-
-config = ConfigParser(interpolation=ExtendedInterpolation())
+# inicializar no primeiro `import` do pacote
+CONFIG = ConfigParser(interpolation=ExtendedInterpolation())
 for arquivo in diretorio_execucao().arquivos:
     if not arquivo.endswith(".ini"): continue
-    config.read(nome_base(arquivo), encoding="utf-8")
+    CONFIG.read(nome_base(arquivo), encoding="utf-8")
 
-
-opcoes_secao = config.options
-obter_secoes = config.sections
-possui_opcao = config.has_option
-possui_secao = config.has_section
-
+opcoes_secao = CONFIG.options
+obter_secoes = CONFIG.sections
+possui_opcao = CONFIG.has_option
+possui_secao = CONFIG.has_section
 
 def possui_opcoes (secao: str, opcoes: Iterable[str]) -> bool:
-    """Versão do `possui_opcao` que aceita uma lista de `opcoes`"""
-    return all(config.has_option(secao, opcao) 
-               for opcao in opcoes)
-
+    """Versão do `possui_opção` que aceita uma lista de `opções`"""
+    return all(
+        CONFIG.has_option(secao, opcao)
+        for opcao in opcoes
+    )
 
 def obter_opcao_ou[T: primitivo] (secao: str, opcao: str, default: T = "") -> T:
     """Obter `opcao` de uma `secao` do configfile ou `default` caso não exista
-    - Função genérica"""
-    return transformar_tipo(config.get(secao, opcao), type(default)) \
+    - Transforma o tipo da variável para o mesmo tipo do `default` informado"""
+    return transformar_tipo(CONFIG.get(secao, opcao), type(default)) \
         if possui_secao(secao) and possui_opcao(secao, opcao) else default
-
 
 def obter_opcoes (secao: str, opcoes: Iterable[str]) -> tuple[str, ...]:
     """Obter `opções` de uma `seção` do configfile
-    - Versão do `obter_opcao` que aceita uma lista de `opcoes`
-    - `AssertionError` caso a `secao` ou alguma `opcao` não exista
-    - `tuple` de retorno terá os valores na mesma ordem que as `opcoes`"""
+    - Versão do `obter_opção` que aceita uma lista de `opções`
+    - `AssertionError` caso a `seção` ou alguma `opção` não exista
+    - `tuple` de retorno terá os valores na mesma ordem que as `opções`"""
     assert possui_secao(secao), f"Seção do configfile '{secao}' não foi configurada"
     assert possui_opcoes(secao, opcoes), f"Variáveis do configfile {str(opcoes)} não foram configuradas para a seção '{secao}'"
-    return tuple(obter_opcao_ou(secao, opcao) for opcao in opcoes)
-
+    return tuple(
+        CONFIG.get(secao, opcao)
+        for opcao in opcoes
+    )
 
 __all__ = [
     "opcoes_secao",
