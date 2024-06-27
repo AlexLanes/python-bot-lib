da047913569e1eb784a15c69bbc0f401613a4311
Alex Lanes <alex_lanes@hotmail.com>
2024-06-27 10:12:06 -0300

    Refatoramento do módulo teclado

--- bot/teclado/__setup.py
+++ bot/teclado/__setup.py
@@ -8,81 +8,73 @@ import bot
 import pyperclip
 from pynput.keyboard import Controller, Key, Listener
 
+TECLADO = Controller()
+CALLBACKS: dict[str, Callable[[], None]] = {}
 
-teclado = Controller()
-callbacks_observador: dict[str, Callable[[], None]] = {}
-
-
-def apertar_tecla (tecla: bot.tipagem.BOTOES_TECLADO, quantidade=1, delay=0.5) -> None:
+def apertar_tecla (tecla: bot.tipagem.BOTOES_TECLADO, quantidade=1, delay=0.1) -> None:
     """Apertar e soltar uma tecla `qtd` vezes
     - `tecla` pode ser do `BOTOES_TECLADO` ou um `char`"""
-    # obter tecla no Enum(Key) se existir, se não char
-    tecla: Key | str = Key[tecla] if any(tecla == k.name for k in Key) else tecla[0]
-    for _ in range(max(quantidade, 1)): 
-        teclado.tap(tecla)
+    tecla: Key | str = tecla if len(tecla) == 1 else Key[tecla]
+    for _ in range(max(quantidade, 1)):
+        TECLADO.tap(tecla)
         sleep(delay)
-
 
 def atalho_teclado (teclas: list[bot.tipagem.BOTOES_TECLADO], delay=0.5) -> None:
     """Apertar as `teclas` sequencialmente e depois soltá-las em ordem reversa
     - `tecla` pode ser do `BOTOES_TECLADO` ou um `char`"""
-    # obter teclas do Enum(Key) se existir, se não char
-    teclas = [Key[tecla] if any(tecla == k.name for k in Key) else tecla[0] for tecla in teclas]
-    for tecla in teclas: teclado.press(tecla) # pressionar teclas
-    for tecla in reversed(teclas): teclado.release(tecla) # soltar teclas
+    teclas: list[Key | str] = [
+        tecla if len(tecla) == 1 else Key[tecla]
+        for tecla in teclas
+    ]
+    for tecla in teclas: TECLADO.press(tecla) # pressionar
+    for tecla in reversed(teclas): TECLADO.release(tecla) # soltar
     sleep(delay)
 
-
-def digitar_teclado (texto: str, delay=0.02) -> None:
+def digitar_teclado (texto: str, delay=0.05) -> None:
     """Digitar o texto pressionando cada tecla do texto e soltando em seguida"""
     for char in texto:
-        teclado.type(char)
+        TECLADO.type(char)
         sleep(delay)
-
 
 def copiar_texto (texto: str) -> None:
     """Substituir o texto copiado da área de transferência pelo `texto`"""
     pyperclip.copy(texto)
 
-
 def colar_texto_copiado () -> None:
     """Realizar ação `CTRL + V` com o texto copiado da área de transferência"""
     atalho_teclado(["ctrl", "v"])
 
-
-def obter_texto_copiado (usoUnico=False) -> str:
+def texto_copiado (apagar=False) -> str:
     """Obter o texto copiado da área de transferência
-    - `usoUnico` determina se o texto será apagado após ser obtido"""
+    - `apagar` determina se o texto será apagado após ser obtido"""
     texto = pyperclip.paste()
-    if usoUnico: copiar_texto("")
+    if apagar: copiar_texto("")
     return texto
-
 
 def observar_tecla (tecla: bot.tipagem.BOTOES_TECLADO, callback: Callable[[], None]) -> None:
     """Observar quando a `tecla` é apertada e chamar o `callback`
     - `tecla` pode ser do `BOTOES_TECLADO` ou um `char`"""
-    callbacks_observador[tecla] = callback
+    CALLBACKS[tecla] = callback
 
     # observador já iniciado
-    if len(callbacks_observador) > 1: return
+    if len(CALLBACKS) > 1: return
 
     # iniciar observador
     def on_press (tecla: Key | str) -> None:
         tecla: str = tecla.name if isinstance(tecla, Key) else str(tecla).strip("'")
-        callback = callbacks_observador.get(tecla, lambda: None)
+        callback = CALLBACKS.get(tecla, lambda: None)
         callback()
 
     observador = Listener(on_press)
     observador.start()
     register(observador.stop)
 
-
 __all__ = [
     "copiar_texto",
+    "texto_copiado",
     "apertar_tecla",
     "atalho_teclado",
     "observar_tecla",
     "digitar_teclado",
-    "obter_texto_copiado",
     "colar_texto_copiado"
 ]
