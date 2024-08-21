d8169cd61ef97491e087e9b2ccb39ef37b13175c
Alex Lanes <alex_lanes@hotmail.com>
2024-08-21 14:09:56 -0300

    Comentário configfile sobre o char #

--- bot/configfile/__init__.py
+++ bot/configfile/__init__.py
@@ -1,7 +1,7 @@
 """Módulo para inicialização de variáveis a partir de arquivo de configuração
 - Para concatenação de valores, utilizar a sintaxe `${opção} ou ${seção:opção}`
 - `$` é reservado, utilizar `$$` para contornar
-- `#` comenta a linha se estiver no começo
+- `#` é reservado para comentar a linha, colocar o valor entre aspas
 - Arquivo terminado em `.ini` deve estar presente na raiz do projeto
     - Caso o `bot.configurar_diretorio()` não tenha sido utilizado, o diretório de execução será utilizado
 
