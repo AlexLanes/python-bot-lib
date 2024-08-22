00245b7f20aa68ef43ba678a24c06abbffc07589
Alex Lanes <alex_lanes@hotmail.com>
2024-08-22 11:05:05 -0300

    Patches no módulo navegador e windows

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bot",
-    version="1.6.1",
+    version="1.6.2",
     packages=find_packages(),
     author="Alex Lanes Angelo",
     author_email="alex_lanes@hotmail.com",
