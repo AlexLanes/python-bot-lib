2d150c0db18503d339543fe316372b894f35f253
Alex Lanes <alex_lanes@hotmail.com>
2024-09-03 10:34:22 -0300

    Atualizado comentário sobre o char "#"

--- bot/configfile/__init__.py
+++ bot/configfile/__init__.py
@@ -1,7 +1,7 @@
 """Módulo para inicialização de variáveis a partir de arquivo de configuração
 - Para concatenação de valores, utilizar a sintaxe `${opção} ou ${seção:opção}`
 - `$` é reservado, utilizar `$$` para contornar
-- `#` é reservado para comentar a linha, colocar o valor entre aspas
+- `#` comenta a linha apenas se tiver no começo
 - Arquivo terminado em `.ini` deve estar presente na raiz do projeto
     - Caso o `bot.configurar_diretorio()` não tenha sido utilizado, o diretório de execução será utilizado
 
@@ -13,11 +13,11 @@ flag_persistencia = False
 dias_persistencia = 14
 
 [FTP]
-host = test.rebex.net
-user = demo 
-password = password
-timeout = 5
-port = 21
+host: test.rebex.net
+user: demo 
+password: password
+timeout: 5
+port: 21
 ```"""
 
 from .__setup import *
