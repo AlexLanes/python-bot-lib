f253649b574c2d1f1e8b5c659d1eb3aeec80d975
Alex Lanes <alex_lanes@hotmail.com>
2024-09-10 14:31:13 -0300

    Alterado descrição e adicionado a variável ini

--- bot/navegador/__init__.py
+++ bot/navegador/__init__.py
@@ -1,13 +1,18 @@
 """Módulo para navegadores Web
-- Abstração do `Selenium`
 
-```
 # Navegadores
 Edge, Chrome e Explorer
+
 # Util
 - Teclas: Teclas especiais para se enviar no método send_keys() de um WebElement
 - ErroNavegador: Classe base para todas as exceções do `Selenium`
 - ElementoNaoEncontrado: Exceção quando um elemento não é encontrado pelo método `encontrar_elemento`
-```"""
+
+# Variáveis .ini
+- `[navegador] -> caminho_perfil` 
+    - `Opcional` informar um caminho de perfil para o `Chrome` ou `Edge` (Carrega as extensões do perfil)
+    - `Default` criado um perfil vazio e apagado após fechar
+    - `chrome://version/` para ver o perfil de execução de um usuário
+"""
 
 from bot.navegador.setup import *
