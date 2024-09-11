a5075a82a78df342d98fe4bf57cc5135d37fee4a
Alex Lanes <alex_lanes@hotmail.com>
2024-09-11 15:25:08 -0300

    Alterado descrição do PORCENTAGENS

--- bot/tipagem/setup.py
+++ bot/tipagem/setup.py
@@ -33,7 +33,7 @@ DIRECOES_SCROLL = Literal["cima", "baixo"]
 BOTOES_MOUSE = Literal["left", "middle", "right"]
 """Botões aceitos pelo `pynput`"""
 PORCENTAGENS = Literal["0.9", "0.8", "0.7", "0.6", "0.5", "0.4", "0.3", "0.2", "0.1"]
-"""Porcentagens de confiança aceitos pelo `PyScreeze` e `EasyOCR`, entre 1.0 e 0.0"""
+"""Porcentagens (float) entre 0.0 e 1.0"""
 ESTRATEGIAS_WEBELEMENT = Literal["id", "xpath", "link text", "name", "tag name", "class name", "css selector", "partial link text"]
 """Estratégias para a localização de WebElements no `Selenium`"""
 BOTOES_TECLADO = Literal["alt", "alt_l", "alt_r", "alt_gr", "backspace", "caps_lock", "cmd", "cmd_r", "ctrl", "ctrl_l", "ctrl_r", "delete", "down", "end", "enter", "esc", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15", "f16", "f17", "f18", "f19", "f20", "f21", "f22", "f23", "f24", "home", "left", "page_down", "page_up", "right", "shift", "shift_r", "space", "tab", "up", "media_play_pause", "media_volume_mute", "media_volume_down", "media_volume_up", "media_previous", "media_next", "insert", "menu", "num_lock", "pause", "print_screen", "scroll_lock"]
