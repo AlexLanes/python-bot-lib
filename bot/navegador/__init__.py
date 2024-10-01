a65413fb75392cecc1e8f841e4e1ace89d9bd55b
Alex Lanes <alex_lanes@hotmail.com>
2024-10-01 06:54:40 -0300

    Alterado descrição do módulo

--- bot/navegador/__init__.py
+++ bot/navegador/__init__.py
@@ -7,10 +7,6 @@ Edge, Chrome e Explorer
 - Teclas: Teclas especiais para se enviar no método send_keys() de um WebElement
 - ErroNavegador: Classe base para todas as exceções do `Selenium`
 - ElementoNaoEncontrado: Exceção quando um elemento não é encontrado pelo método `encontrar_elemento`
-
-# Variáveis .ini
-- `[navegador] -> caminho_extensoes` 
-    - `Opcional` caminhos para diretório de extensões, separados por vírgula, para utilizar no `Chrome` ou `Edge`
 """
 
 from bot.navegador.setup import *
