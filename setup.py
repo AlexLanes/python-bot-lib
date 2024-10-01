a1eb3e4142ed3abd875c7e1bcb035908d5c148d0
Alex Lanes <alex_lanes@hotmail.com>
2024-10-01 06:58:22 -0300

    Nova versão com alteração no navegador

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="bot",
-    version="2.1",
-    packages=find_packages(),
-    author="Alex Lanes Angelo",
-    author_email="alex_lanes@hotmail.com",
-    description="Pacote com funcionalidades gerais para criação de Bots",
-    url="https://github.com/AlexLanes/python-bot-lib",
-    python_requires=">=3.12",
-    include_package_data=True,
-    install_requires=[
+    name = "bot",
+    version = "2.2",
+    packages = find_packages(),
+    author = "Alex Lanes Angelo",
+    author_email = "alex_lanes@hotmail.com",
+    description = "Pacote com funcionalidades gerais para criação de Bots",
+    url = "https://github.com/AlexLanes/python-bot-lib",
+    python_requires = ">=3.12",
+    include_package_data = True,
+    install_requires = [
         linha.split("#")[0].strip()
         for linha in open("./requirements.txt", "r", encoding="utf-8").readlines()
         if not linha.isspace() and not linha.startswith("#")
     ],
-    package_data={
+    package_data = {
         "bot": ["sistema/QRes.exe"],
     },
-    extras_require={
+    extras_require = {
         "ocr": ["easyocr"],
-    },
+    }
 )
