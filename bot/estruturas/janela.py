901f62ba5f91c17feb186a32f07605cc22da0d46
Alex Lanes <alex_lanes@hotmail.com>
2024-12-18 15:02:19 -0300

    Refatoramento e adicionado método encerrar e alterado o fechar

--- bot/estruturas/janela.py
+++ bot/estruturas/janela.py
@@ -7,6 +7,7 @@ from . import Coordenada
 from .. import util, tipagem
 # externo
 from pywinauto.timings import Timings
+from pywinauto.win32structures import RECT
 from pywinauto import Application, Desktop, mouse
 from pywinauto.controls.hwndwrapper import HwndWrapper
 
@@ -28,10 +29,10 @@ class Janela:
     """Classe de interação com as janelas abertas. 
     - Abstração do `pywinauto`"""
 
-    __janela: HwndWrapper
-    """Conexão com a janela superior"""
-    __aplicacao: Application
-    """Application da janela"""
+    elemento: HwndWrapper
+    """Elemento da janela"""
+    aplicacao: Application
+    """Application superior da janela"""
 
     def __init__ (self, titulo: str | None = None,
                         class_name: str | None = None,
@@ -41,8 +42,8 @@ class Janela:
         - `backend` varia de acordo com a janela, testar com ambos para encontrar o melhor"""
         if not titulo and not class_name:
             handle = ctypes.windll.user32.GetForegroundWindow()
-            self.__janela = Desktop(backend).window(handle=handle)
-            self.__aplicacao = Application(backend).connect(handle=handle)
+            self.elemento = Desktop(backend).window(handle=handle).wrapper_object()
+            self.aplicacao = Application(backend).connect(handle=handle)
             return
 
         titulos_janelas = self.titulos_janelas()
@@ -53,14 +54,13 @@ class Janela:
             if titulo_normalizado in util.normalizar(titulo_janela)
         ] or [None])[0]
 
-        self.__janela = Desktop(backend).window(title=titulo, class_name=class_name, visible_only=True)
-        self.__aplicacao = Application(backend).connect(title=titulo, class_name=class_name, visible_only=True)
+        self.elemento = Desktop(backend).window(title=titulo, class_name=class_name, visible_only=True).wrapper_object()
+        self.aplicacao = Application(backend).connect(title=titulo, class_name=class_name, visible_only=True)
 
     def __eq__ (self, other: Janela) -> bool:
         """Comparar se o handler de uma janela é o mesmo que a outra"""
         if not isinstance(other, Janela): return False
-        return self.__janela.handle == other.__janela.handle
-
+        return self.elemento.handle == other.elemento.handle
     def __repr__ (self) -> str:
         """Representação da classe"""
         return f"<Janela '{self.titulo}'>"
@@ -68,46 +68,49 @@ class Janela:
     @property
     def titulo (self) -> str:
         """Titulo da janela"""
-        return self.__janela.window_text()
+        return self.elemento.window_text()
     @property
     def maximizada (self) -> bool:
         """Checar se a janela está maximizada"""
-        return self.__janela.is_maximized()
+        return self.elemento.is_maximized()
     @property
     def minimizada (self) -> bool:
         """Checar se a janela está minimizada"""
-        return self.__janela.is_minimized()
+        return self.elemento.is_minimized()
     @property
     def focada (self) -> bool:
         """Checar se a janela está focada"""
-        return self.__janela.is_active()
+        return self.elemento.is_active()
     @property
     def coordenada (self) -> Coordenada:
         """Coordenada da janela
         - `Coordenada` zerada caso a janela esteja minimizada"""
-        box = self.__janela.rectangle()
+        box: RECT = self.elemento.rectangle()
         return Coordenada(box.left, box.top, box.width(), box.height())
 
     def minimizar (self) -> Self:
         """Minimizar janela"""
-        self.__janela.minimize()
+        self.elemento.minimize()
         return self
     def maximizar (self) -> Self:
         """Maximizar janela"""
-        self.__janela.maximize()
+        self.elemento.maximize()
         return self
     def restaurar (self) -> Self:
         """Restaurar a janela para o estado anterior"""
-        self.__janela.restore()
+        self.elemento.restore()
         return self
     def focar (self) -> Self:
         """Focar na janela"""
         if self.minimizada: self.restaurar()
-        self.__janela.set_focus()
+        self.elemento.set_focus()
         return self
     def fechar (self) -> None:
-        """Fechar janela"""
-        self.__aplicacao.kill()
+        """Fechar a janela atual"""
+        self.elemento.close(5)
+    def encerrar (self) -> None:
+        """Encerrar o processo da aplicação forçadamente"""
+        self.aplicacao.kill()
 
     def elementos (self, *, title: str = None, title_re: str = None,
                    class_name: str = None, control_id: int = None,
@@ -115,9 +118,9 @@ class Janela:
         """Obter uma lista elementos com base nos parâmetros informados
         - O tipo do retorno pode ser diferente dependendo do tipo do backend e controle
         - Retornado uma classe genérica que compartilham múltiplos métodos"""
-        return self.__aplicacao.windows(title=title, title_re=title_re, class_name=class_name,
-                                        control_id=control_id, top_level_only=top_level_only,
-                                        visible_only=visible_only, enabled_only=enabled_only)
+        return self.aplicacao.windows(title=title, title_re=title_re, class_name=class_name,
+                                      control_id=control_id, top_level_only=top_level_only,
+                                      visible_only=visible_only, enabled_only=enabled_only)
 
     @staticmethod
     def titulos_janelas () -> set[str]:
