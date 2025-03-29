68e857029c0b2fa39a24c39d7ec48ffddcd3a5b8
Alex Lanes <alex_lanes@hotmail.com>
2025-03-28 23:07:51 -0300

    Versão 2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "bot",
-    version = "2.4",
+    version = "2.5",
     packages = find_packages(),
     author = "Alex Lanes Angelo",
     author_email = "alex_lanes@hotmail.com",
