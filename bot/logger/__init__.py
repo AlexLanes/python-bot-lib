8a1da534f28c8aa92423fe79d754e2f118ff537d
Alex Lanes <alex_lanes@hotmail.com>
2025-08-17 07:13:14 -0300

    Movido descrição para a variável

--- bot/logger/__init__.py
+++ bot/logger/__init__.py
@@ -1,7 +1,3 @@
-"""Pacote para realizar e tratar Logs
-- Cria um LOG na raiz do projeto para fácil acesso `caminho_log_raiz()`
-- Salva o LOG no diretório de persistência se requisitado `caminho_log_persistencia()`
-- Caso o `bot.configurar_diretorio()` não tenha sido utilizado, o diretório de execução será utilizado
-- Variáveis .ini `[logger] -> [dias_persistencia: 14, flag_persistencia: True, flag_debug: False]`"""
+"""Pacote para realizar e tratar Logs"""
 
 from bot.logger.setup import *
